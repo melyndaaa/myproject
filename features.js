7f5e93bd6f05743a216150e4400e9babe6bef606
melynda <melynda2097@gmail.com>
2024-10-01 19:08:01 +0100

    created a website for pookie

---
+++ features.js
@@ -0,0 +1,11 @@
+alert("Welcome, pookie!");
+
+function showHappyFace() {
+    document.getElementById("faceContainer1").innerHTML = "Love you :3";
+    document.getElementById("faceContainer1").classList.add("happy-face");
+}
+
+function showHappyFaceifno() {
+    document.getElementById("faceContainer2").innerHTML = "Yes!! 😊";
+    document.getElementById("faceContainer2").classList.add("happy-face");
+}
